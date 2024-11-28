cab39b5f663c7e7576286352edff3c30adaeadc2
Ольга1213 <O_Mischenkova@mail.ru>
2024-11-28 23:41:16 +0500

    commit1

--- src/Main.java
+++ src/Main.java
@@ -23,70 +23,70 @@ public class Main {
             System.out.println(n + " ");
         }
         System.out.println();
-    }
 
-    //3
-    double population = 12000000;
-    int birthRate = 17;
-    int deathRate = 8;
-    for(
-    int year = 1; year <=10;year++)
 
-    {
-        double birth = population / 1000 * birthRate;
-        double death = population / 1000 * deathRate;
-        population = population + birth - death;
-        System.out.println("Год" + year + ",численность населения составляет" + (long) population);
-    };
-
-    //4
-    sum =15000;
-    month =1;
-    while(sum<12000000);
-
-    {
-        sum = sum + sum * 7 / 100;
-        month++;
-        System.out.println("месяц=" + month + " сумма =" + (int) sum);
-    }
-
-    //5
-    sum=15000;
-    month=1;
-    while(sum<12000000);
-
-    {
-        sum = sum + sum * 7 / 100;
-        month++;
-        if (month % 6 == 0) {
-            System.out.println("месяц=" + month + " сумма =" + (int) sum);
+        //3
+        double population = 12000000;
+        int birthRate = 17;
+        int deathRate = 8;
+        for (
+                int year = 1; year <= 10; year++) {
+            double birth = population / 1000 * birthRate;
+            double death = population / 1000 * deathRate;
+            population = population + birth - death;
+            System.out.println("Год" + year + ",численность населения составляет" + (long) population);
         }
-        //6
+        ;
+
+        //4
         sum = 15000;
         month = 1;
-        while (month < 9 * 12) {
+        while (sum < 12000000) ;
+
+        {
             sum = sum + sum * 7 / 100;
             month++;
-            if (month % 6 == 0) ;
-            {
+            System.out.println("месяц=" + month + " сумма =" + (int) sum);
+        }
+
+        //5
+        sum = 15000;
+        month = 1;
+        while (sum < 12000000) ;
+
+        {
+            sum = sum + sum * 7 / 100;
+            month++;
+            if (month % 6 == 0) {
                 System.out.println("месяц=" + month + " сумма =" + (int) sum);
             }
-
-            //7
-            int friday = 1;
-            while (friday <= 31) {
-                System.out.println("Сегодня пятница," + friday + "-е число. Необходимо подготовить отчет");
-                friday += 7;
-            }
-
-            //8
-
-            for (int year = 2024 - 200; year < 2024 + 100; year++) {
-                if (year % 79 == 0) {
-                    System.out.println(year);
+            //6
+            sum = 15000;
+            month = 1;
+            while (month < 9 * 12) {
+                sum = sum + sum * 7 / 100;
+                month++;
+                if (month % 6 == 0) ;
+                {
+                    System.out.println("месяц=" + month + " сумма =" + (int) sum);
                 }
-            }
 
+                //7
+                int friday = 1;
+                while (friday <= 31) {
+                    System.out.println("Сегодня пятница," + friday + "-е число. Необходимо подготовить отчет");
+                    friday += 7;
+                }
+
+                //8
+
+                for (int year = 2024 - 200; year < 2024 + 100; year++) {
+                    if (year % 79 == 0) {
+                        System.out.println(year);
+                    }
+                }
+
+            }
         }
     }
 }
